1a9b2e9886600a9c779e30ed55cf750078379091
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-08-13 14:45:33 +0900

    Delete default.php

--- notification/mails/plaintext/default.php
+++
@@ -1,7 +0,0 @@
-<?php $this->beginContent('@notification/views/layouts/mail_plaintext.php', $_params_); ?>
-
-<?= $text; ?>
-
-
-<?= strip_tags(Yii::t('NotificationModule.views_notificationLayoutMail', 'View online:')); ?> <?php echo urldecode($url); ?>
-<?php $this->endContent(); ?>
